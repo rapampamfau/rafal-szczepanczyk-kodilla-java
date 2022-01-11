badb92261ae57a7e3d2a82515ad34a217b327ae8
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-01-11 20:50:21 +0100

    6.2 Added Calculator class and tested it in TestingMain

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,14 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
+        //SimpleUser tests
         SimpleUser simpleUser = new SimpleUser("theForumUser");
 
         String result = simpleUser.getUsername();
 
         if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Calculator tests
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(7, 15);
+        int subtractResult = calculator.subtract(7, 2);
+
+        //add test
+        if (addResult == 22) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //subtract test
+        if (subtractResult == 5) {
             System.out.println("test OK");
         } else {
             System.out.println("Error!");
