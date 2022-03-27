de325efced36e47e30d75f518d353ea29bc0504c
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-03-27 15:34:11 +0200

    14.4 Spring web module

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,15 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Test;
+
+public class LoggerTestSuite {
+
+    private static Logger logger;
+
+    @Test
+    void testGetLastLog() {
+        //Given
+        //When
+        //Then
+    }
+}
