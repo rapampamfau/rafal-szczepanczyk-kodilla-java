241dcf576f27a0b0bc04a67454a3f5ebdbc0227a
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-04-07 21:16:23 +0200

    16.4 Displaying tasks in tasks lists

--- kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -1,2 +1,21 @@
-package com.kodilla.testing.loop;public class testLoopAndConditionalBreakpoints {
+package com.kodilla.testing.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testLoopAndConditionalBreakpoints {
+
+    @Test
+    void testLoop() {
+        //Given
+        long sum = 0;
+
+        //When
+        for (int n = 0; n < 1000; n++) {
+            sum += n;
+            System.out.println("[" + n + "] Sum equals: " + sum);
+        }
+        //Then
+        assertEquals(499500, sum);
+    }
 }
