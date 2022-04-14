0e6b9bbd44cc2e5fb8eadea7b48ea6bf29ab1a86
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-04-14 14:31:46 +0200

    17.1 Spring Hibernate module

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuRow.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuRow.java
@@ -1,13 +1,2 @@
-package com.kodilla.sudoku;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class SudokuRow {
-
-    private List<SudokuElement> row = new ArrayList<>();
-
-    public List<SudokuElement> getRow() {
-        return row;
-    }
+package com.kodilla.sudoku;public class SudokuRow {
 }
